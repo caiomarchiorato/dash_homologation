71c7bdea7c6eaa5e07edef3cddbb6a28d3711049
Caio <cmarchiorato@hotmail.com>
2024-03-18 17:27:01 -0300

    working

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ from utils.query_execution import create_dataframe_from_query, execute_query
 from utils.data_processing import create_histogram 
 
 col_name, _ = execute_query('data/queries/')
+query_data = create_dataframe_from_query('data/queries/')
 
 #instanciando o app
 app = Dash(__name__)
-app.layout = html.Div(children=[
-    create_table(),  # adicionando a tabela ao layout
 
+app.layout = html.Div(children=[
     html.Div([
         dcc.Dropdown(col_name, 'produtividade', id='column-dropdown', placeholder='Select a coluna'),
             html.Div(id='table-container'
@@ -23,7 +23,9 @@ app.layout = html.Div(children=[
     html.Div(id='histogram-container', children=[
         html.H1('Histogram'),
         dcc.Graph(id='histogram')
-    ])
+    ]),
+    
+    create_table()
 ])
 
 @app.callback(
@@ -33,19 +35,17 @@ def update_output(value):
     return f'You have selected {value}'
 
 @app.callback(
-    Output('histogram', 'children'),
+    Output('histogram', 'figure'),
     [Input('column-dropdown', 'value')])
 def update_histograms(selected_columns):
     if selected_columns:
-        fig = create_histogram(create_dataframe_from_query('data/queries/'), selected_columns)
-        histogram_div = html.Div(children=[
-            html.H2(f'Histogram of {selected_columns}'),
-            dcc.Graph(figure=fig)
-        ])
-        return histogram_div
+        fig = create_histogram(query_data, selected_columns)
+        print(f'Coluna selecionada {selected_columns}')
+        return fig
     else:
-        return [html.H1('Histogram')]
-
-
+        print('No column selected')
+        return {'data': [], 'layout': {}}
+    
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
